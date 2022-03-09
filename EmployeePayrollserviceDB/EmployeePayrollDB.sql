e818ee6a5eda95cbc58cce09cf57370e5cb53615
sunilpowar <sunilpowarsp8468@gmail.com>
2022-03-09 21:31:57 +0530

    Adding UC2

--- EmployeePayrollserviceDB/EmployeePayrollDB.sql
+++ EmployeePayrollserviceDB/EmployeePayrollDB.sql
@@ -4,3 +4,12 @@ Welcome To The Employee PayRoll Service DataBase
 
 create database Payroll_Service;
 use PayRoll_Service;
+
+#UC 2  Create Table
+
+CREATE TABLE Employee_Payroll (
+    Id int identity(1,1) primary key,
+    Name varchar(255) not null,
+    Salary float,
+    StartDate date,
+); 
