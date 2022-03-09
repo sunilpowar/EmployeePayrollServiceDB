6cd9e16bf59bbae9a6ce36cc18c1fe886c27c7ee
sunilpowar <sunilpowarsp8468@gmail.com>
2022-03-10 00:54:47 +0530

    Adding UC6

--- EmployeePayrollserviceDB/EmployeePayrollDB.sql
+++ EmployeePayrollserviceDB/EmployeePayrollDB.sql
@@ -18,10 +18,11 @@ CREATE TABLE Employee_Payroll (
 
 insert into employee_payroll (Name, Salary, StartDate) values
 ('Sunil', 25000.00, '2012-05-09'),
-('Sourabh', 35000.00, '2014-12-06'),
+('Sushama', 35000.00, '2014-12-06'),
 ('Karan', 40000.00, '2010-07-05'),
 ('Prafull', 65000.00, '2008-12-25'),
-('Rohan', 22000.00, '2016-01-31');
+('Rohan', 22000.00, '2016-01-31'),
+('Snehal', 60000.00, '2021-11-19');
 
 --# UC 4  Retreive Records Using Select--
 
@@ -33,3 +34,9 @@ SELECT Salary FROM Employee_Payroll WHERE NAME='Karan'
 SELECT Salary,Name FROM Employee_Payroll WHERE Name LIKE 'K%';
 SELECT Salary,Name FROM Employee_Payroll WHERE StartDate between CAST('2010-07-05' as Date) and getdate(); 
 SELECT Salary,Name FROM Employee_Payroll WHERE Salary between 35000 and 45000; 
+
+--#UC 6  Alter And Update Data
+
+Alter Table Employee_Payroll Add Gender	char(1);
+update Employee_Payroll set Gender='M' where Id IN (1,3,4,5);
+update Employee_Payroll set Gender='F' where Id IN (2,6);
